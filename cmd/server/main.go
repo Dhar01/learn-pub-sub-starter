1822f67d8b6c3bf207b3c259e268bd150752df3d
Loknath Dhar <loknath-dhar@pm.me>
2025-03-29 16:46:39 +0600

    sending publish message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,11 +18,23 @@ func main() {
 
 	defer conn.Close()
 
+	chnl, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("can't create a new channel: %v", err)
+	}
+
+	if err = pubsub.PublishJSON(chnl, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	}); err != nil {
+		log.Fatalf("can't publish JSON: %v", err)
+	}
+
 	log.Println("Peril game server connected to RabbitMQ!")
 
-	// shutting down
-	defer fmt.Println("system is shutting down...")
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	// // shutting down
+	// defer fmt.Println("system is shutting down...")
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+
 }
