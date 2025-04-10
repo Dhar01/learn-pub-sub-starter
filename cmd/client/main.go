b333e72f5ce460653794c71e3bcc995414c0c447
Loknath Dhar <loknath-dhar@pm.me>
2025-04-10 12:50:51 +0600

    updating client to declare and bind the queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril client...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("amqp connection error: %v", err)
+	}
+
+	// defer conn.Close()
+	// log.Println("Peril game server connected to RabbitMQ!")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("can't get the username: %v", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.Transient)
+	if err != nil {
+		log.Fatalf("can't get channel and queue. Error: %v", err)
+	}
+
+	// shutting down
+	defer fmt.Println("system is shutting down...")
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -7,6 +7,11 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+const (
+	Transient = 0
+	Durable   = 1
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	valByte, err := json.Marshal(val)
 	if err != nil {
@@ -26,4 +31,43 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	}
 
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	simpleQueueType int,
+) (*amqp.Channel, amqp.Queue, error) {
+	chnl, err := conn.Channel()
+	if err != nil {
+		return wrapDeclareBindError(err)
+	}
+
+	durable := true
+	autoDelete := false
+	exclusive := false
+
+	// if queuetype is transient
+	if simpleQueueType == Transient {
+		durable = false
+		autoDelete = true
+		exclusive = true
+	}
+
+	queue, err := chnl.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		return wrapDeclareBindError(err)
+	}
+
+	if err = chnl.QueueBind(queueName, key, exchange, false, nil); err != nil {
+		return wrapDeclareBindError(err)
+	}
+
+	return chnl, queue, nil
+}
+
+func wrapDeclareBindError(err error) (*amqp.Channel, amqp.Queue, error) {
+	return &amqp.Channel{}, amqp.Queue{}, err
 }
