45e04b248b70c211f7c1d306c5c5211c5f8e2e6f
Loknath Dhar <loknath-dhar@pm.me>
2025-04-10 13:16:11 +0600

    adding clinet REPL command

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -33,10 +30,48 @@ func main() {
 		log.Fatalf("can't get channel and queue. Error: %v", err)
 	}
 
-	// shutting down
-	defer fmt.Println("system is shutting down...")
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	state := gamelogic.NewGameState(username)
+
+	for {
+		inp := gamelogic.GetInput()
+
+		if len(inp) <= 0 {
+			log.Println("Enter specific commands")
+		}
+
+		if inp[0] == "spawn" {
+			if len(inp) != 3 {
+				log.Println("invalid command format")
+			}
+
+			if err := state.CommandSpawn(inp); err != nil {
+				log.Println("Error:", err)
+			}
+		} else if inp[0] == "move" {
+			if len(inp) != 3 {
+				log.Println("invalid command format")
+			}
+
+			if _, err := state.CommandMove(inp); err != nil {
+				log.Println("Error:", err)
+			}
+		} else if inp[0] == "status" {
+			state.CommandStatus()
+		} else if inp[0] == "help" {
+			gamelogic.PrintClientHelp()
+		} else if inp[0] == "spam" {
+			log.Println("Spamming not allowed yet!")
+		} else if inp[0] == "quit" {
+			gamelogic.PrintQuit()
+		} else {
+			log.Println("invalid command input")
+		}
+	}
+
+	// // shutting down
+	// defer fmt.Println("system is shutting down...")
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
 
 }
