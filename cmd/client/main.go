0fc58f79a0bfdb1a22ef384f9f85d79f893cd8a3
Loknath Dhar <loknath-dhar@pm.me>
2025-04-25 16:07:16 +0600

    update

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -26,15 +26,47 @@ func main() {
 		log.Fatalf("can't get the username: %v", err)
 	}
 
-	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.Transient)
+	chnl, _, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.Transient,
+	)
 	if err != nil {
 		log.Fatalf("can't get channel and queue. Error: %v", err)
 	}
 
+	// _, _, err = pubsub.DeclareAndBind(
+	// 	conn,
+	// 	routing.ExchangePerilTopic,
+	// 	routing.ArmyMovesPrefix+"."+username,
+	// 	routing.PauseKey,
+	// 	pubsub.Transient,
+	// )
+
 	state := gamelogic.NewGameState(username)
 
-	if err := pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, routing.PauseKey+"."+username, routing.PauseKey, pubsub.Transient, handlerPause(state)); err != nil {
-		log.Fatalf(err.Error())
+	if err = pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilDirect,
+		routing.PauseKey+"."+username,
+		routing.PauseKey,
+		pubsub.Transient,
+		handlerPause(state),
+	); err != nil {
+		log.Fatalf("Subscribe error: %v", err)
+	}
+
+	if err = pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilTopic,
+		routing.ArmyMovesPrefix+"."+username,
+		routing.ArmyMovesPrefix+".*",
+		pubsub.Transient,
+		handlerMove(state),
+	); err != nil {
+		log.Fatalf("Subscribe error: %v", err)
 	}
 
 	for {
@@ -42,24 +74,42 @@ func main() {
 
 		if len(inp) <= 0 {
 			log.Println("Enter specific commands")
+			continue
 		}
 
 		if inp[0] == "spawn" {
 			if len(inp) != 3 {
 				log.Println("invalid command format")
+				continue
 			}
 
 			if err := state.CommandSpawn(inp); err != nil {
 				log.Println("Error:", err)
+				continue
 			}
 		} else if inp[0] == "move" {
 			if len(inp) != 3 {
 				log.Println("invalid command format")
+				continue
 			}
 
-			if _, err := state.CommandMove(inp); err != nil {
+			moveData, err := state.CommandMove(inp)
+			if err != nil {
 				log.Println("Error:", err)
+				continue
 			}
+
+			if err = pubsub.PublishJSON(
+				chnl,
+				routing.ExchangePerilTopic,
+				routing.ArmyMovesPrefix+"."+username,
+				moveData,
+			); err != nil {
+				log.Fatalf("Error: %v", err)
+				continue
+			}
+
+			log.Println("move published successfully")
 		} else if inp[0] == "status" {
 			state.CommandStatus()
 		} else if inp[0] == "help" {
@@ -86,5 +136,12 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 		defer fmt.Print("> ")
 
 		gs.HandlePause(ps)
+	}
+}
+
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(move gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(move)
 	}
 }
